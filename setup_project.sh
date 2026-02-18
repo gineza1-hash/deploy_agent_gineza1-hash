cbf845fa0e7beb22fe64f5818b574f91c5c08d33
gineza1-hash <g.ineza1@alustudent.com>
2026-02-18 17:42:37 +0200

    summative

---
+++ setup_project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir attendance_tracker_v1
+touch attendance_tracker_v1/attendance_checker.py
+mkdir attendance_tracker_v1/Helpers
+touch attendance_tracker_v1/Helpers/assets.csv
+touch attendance_tracker_v1/Helpers/config.json
+mkdir attendance_tracker_v1/reports
+touch attendance_tracker_v1/reports/reports.log
